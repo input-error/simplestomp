a8d60b293282912199f2433f142e304ee816980b
Matt Mix <matthew.w.mix@gmail.com>
2019-07-18 09:54:33 -0400

    Adding in broken out tests.

--- simplestomp_test.go
+++ simplestomp_test.go
@@ -1,58 +1,78 @@
 package simplestomp
 
 import (
+	"context"
 	"github.com/go-stomp/stomp"
 	"testing"
+	"time"
 )
 
-// Starts up a rkt? or docker? container prior to running tests
-func setupDockerContainer() {
-}
-
-func TestSendMessage(t *testing.T) {
-}
-
-func TestGetMessage(t *testing.T) {
-}
-
-func TestProcessMessage(t *testing.T) {
-}
-
-func TestAll(t *testing.T) {
-	svc := &simplestomp{
+func getConnection() *simplestomp {
+	svc := simplestomp{
 		Username: "artemis",
 		Password: "artemis",
 		Server:   "localhost",
 		Port:     61616,
 	}
+
+	return &svc
+}
+
+// Starts up a container using podman or buildah
+func setupContainer() {
+}
+
+func TestSendMessage(t *testing.T) {
+	svc := getConnection()
 	defer svc.Close()
 
 	err := svc.SendMessage("This is a test!", "testqueue", "text/plain")
 	if err != nil {
 		t.Errorf("Crap we failed to send!. Error: %s\n", err)
-		t.Fail()
+		t.FailNow()
 	}
-	t.Logf("Sent first message")
+	t.Logf("Sent first message\n")
+}
+
+func TestGetMessage(t *testing.T) {
+	svc := getConnection()
+	defer svc.Close()
+
+	err := svc.SendMessage("This is a test!", "testqueue", "text/plain")
+	if err != nil {
+		t.Errorf("Crap we failed to send!. Error: %s\n", err)
+		t.FailNow()
+	}
+	t.Logf("Sent first message\n")
 
 	msg, err := svc.GetMessage("testqueue")
 	if err != nil {
 		t.Errorf("Failed to get messages!. Error: %s\n", err)
-		t.Fail()
+		t.FailNow()
 	}
 
 	t.Logf("Message: %s\n", msg)
+}
 
-	err = svc.SendMessage("This is a test 2!", "testqueue", "text/plain")
+func TestProcessMessage(t *testing.T) {
+	svc := getConnection()
+	defer svc.Close()
+
+	err := svc.SendMessage("This is a test 2!", "testqueue", "text/plain")
 	if err != nil {
 		t.Errorf("Crap we failed to send!. Error: %s\n", err)
-		t.Fail()
 	}
 
+	ctx := context.Background()
 	err = svc.ProcessMessages(
 		"testqueue",
+		ctx,
 		func(message *stomp.Message) error {
 			t.Logf("ProcMessages: Msg: %s\n", string(message.Body))
 
 			return message.Err
 		})
+
+	time.Sleep(3)
+	ctx.Done()
 }
